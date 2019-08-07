c1075df504ab711ecd46e9d1c64b3fb296aff050
Donal Donohoe <donaldonohoe@Donals-MBP.Hitronhub.home>
2019-08-07 17:25:50 +0100

    Initial commit

---
+++ dev_theme/assets/js/scripts.js
@@ -0,0 +1,238 @@
+$( document ).ready(function() {
+
+  const desktopBreakPoint = 991;
+
+
+
+
+  // Position mega menus
+  function megaMenuWidth() {
+    if( window.innerWidth > desktopBreakPoint ) { // Desktop only
+      $('.mega-menu').removeClass('active').css('width', $(window).width());
+    }
+    else {
+      $('.mega-menu').css('width', 'auto');
+    }
+	}
+	megaMenuWidth();
+	$(window).resize ( function() {	megaMenuWidth(); });
+
+
+
+
+
+
+
+  // Menu open/close
+  $('#menu-btn').off().click( function(e) {
+    e.preventDefault();
+    $(this).toggleClass('active');
+    $('#main-nav').toggleClass('active');
+    $('#main-nav > ul').removeClass('slide-out'); // Close currency menu
+    // Header state
+    if($('#main-nav').hasClass('active') && !$('header').hasClass('scrolled')) { // if menu open at top
+			$('header').addClass('solid');
+		} else if (!$('#main-nav').hasClass('active') && !$('header').hasClass('scrolled')) { // if menu closed at top
+      $('header').removeClass('solid');
+    } else if ($('#main-nav').hasClass('active') && $('header').hasClass('scrolled')) { // if menu open past top
+      $('header').addClass('solid');
+    } else if ($('#main-nav').hasClass('active') && $('header').hasClass('scrolled')) { // if menu closed past top
+      $('header').addClass('solid');
+    }
+  });
+
+  var mobileMenuSlideTime = 200;
+  // Top level menu items open/close
+  $('#main-nav > ul > li > a').off().click(function(e) {
+    e.preventDefault();
+    // Close sibling menus and inner menus
+    $('#main-nav > ul > li > a').not(this).removeClass('active');
+    $('#main-nav > ul > li > a').not(this).siblings('.mega-menu').slideUp(mobileMenuSlideTime).removeClass('active');
+    $('#main-nav > ul > li > a').not(this).siblings('.mega-menu').children('.inner-list').children('h3').removeClass('active');
+    $('#main-nav > ul > li > a').not(this).siblings('.mega-menu').children('.inner-list').children('ul').slideUp(mobileMenuSlideTime).removeClass('active');
+    // Open clicked menu
+    $(this).toggleClass('active').siblings('.mega-menu').slideToggle(mobileMenuSlideTime).toggleClass('active');
+	});
+  // Inner menu items open/close
+  $('#main-nav .mega-menu .inner-list > h3').off().click(function(e) {
+    e.preventDefault();
+    // Close sibling inner menus
+    $('#main-nav .mega-menu .inner-list > h3').not(this).removeClass('active');
+    $('#main-nav .mega-menu .inner-list > h3').not(this).siblings('ul').slideUp(mobileMenuSlideTime).removeClass('active');
+    // Open clicked menu
+    $(this).toggleClass('active').siblings('ul').slideToggle(mobileMenuSlideTime).toggleClass('active');
+	});
+
+  // Currency open
+  $('.currency-btn').off().click( function() {
+    $('#main-nav > ul').toggleClass('slide-out');
+  });
+  // Currency close
+  $('.currency-close-btn').off().click( function() {
+    $('#main-nav > ul').toggleClass('slide-out');
+  });
+
+
+  // Change header state on scroll
+	function headerScroll() {
+    var scrollTrigger = 10; // pixels from top for scroll state to take action
+	  var scrolled = $(window).scrollTop();
+		if( scrolled > scrollTrigger && $('#main-nav').hasClass('active') ) { // If past top and menu open
+			$('header').addClass('solid scrolled');
+		} else if( scrolled > scrollTrigger && !$('#main-nav').hasClass('active') ) { // If past top and menu closed
+			$('header').addClass('solid scrolled');
+		} else if( scrolled <= scrollTrigger && $('#main-nav').hasClass('active') ) { // If at top and menu open
+			$('header').addClass('solid').removeClass('scrolled');
+		} else if( scrolled <= scrollTrigger && !$('#main-nav').hasClass('active') ) { // If at top and menu closed
+			$('header').removeClass('solid scrolled');
+		}
+	}
+	headerScroll();
+	$(window).scroll ( function() {	headerScroll(); });
+	$(window).resize ( function() {	headerScroll(); });
+
+
+
+  // Hover on top level menu items
+  $('#main-nav > ul > li > a').hover(function(e) {
+    if( window.innerWidth > desktopBreakPoint ) { // Desktop only
+      var thisTopLevelItem = $(this);
+      // Make header solid
+      $('header').addClass('solid');
+      // Show and position menu underline
+      var thisTopLevelItemSidePadding = parseInt(thisTopLevelItem.css('padding-left')) + parseInt(thisTopLevelItem.css('padding-right'));
+      var thisTopLevelItemWidth = parseInt(thisTopLevelItem.innerWidth()) - thisTopLevelItemSidePadding; // From text to end of chevron
+      var thisTopLevelItemPosLeft = parseInt(thisTopLevelItem.position().left) + parseInt(thisTopLevelItem.css('padding-left'));
+      $('#main-nav > ul > .underline').addClass('active').css({
+        'width' : thisTopLevelItemWidth,
+        'left' : thisTopLevelItemPosLeft
+      });
+      // Make this mega-menu active and adjust height of mega-menu background
+      if (thisTopLevelItem.siblings('.mega-menu').length) {
+        thisTopLevelItem.siblings('.mega-menu').addClass('active');
+        var activeMegaMenuHeight = $('.mega-menu.active').outerHeight();
+        $('#mega-menu-bg').addClass('active').css('height', activeMegaMenuHeight);
+      }
+    }
+  },function(e) {
+    if( window.innerWidth > desktopBreakPoint ) { // Desktop only
+      // Reset header if not scrolled
+      if (!$('header').hasClass('scrolled')) {
+        $('header').removeClass('solid');
+      }
+      // Hide menu underline
+      $('#main-nav > ul > .underline').removeClass('active');
+      // Hide this mega menu and mega-menu background
+      $(this).siblings('.mega-menu').removeClass('active');
+      $('#mega-menu-bg').removeClass('active');
+    }
+	});
+
+  // Hover on mega menu
+  $('.mega-menu').hover(function(e) {
+    if( window.innerWidth > desktopBreakPoint ) { // Desktop only
+      // Make header solid
+      $('header').addClass('solid');
+      // Show menu underline
+      $('#main-nav > ul > .underline').addClass('active');
+      // Show this mega menu and mega-menu background
+      $(this).addClass('active');
+      $('#mega-menu-bg').addClass('active');
+    }
+  },function(e) {
+    if( window.innerWidth > desktopBreakPoint ) { // Desktop only
+      // Reset header if not scrolled
+      if (!$('header').hasClass('scrolled')) {
+        $('header').removeClass('solid');
+      }
+      // Hide menu underline
+      $('#main-nav > ul > .underline').removeClass('active');
+      // Hide this mega menu and mega-menu background
+      $(this).removeClass('active');
+      $('#mega-menu-bg').removeClass('active');
+    }
+	});
+
+
+
+
+
+
+
+  // Custom fix for object-cover images in IE 11
+  var isIE11 = !!navigator.userAgent.match(/Trident.*rv\:11\./);
+  if (isIE11 == true && $('img.object-cover').length) {
+    $('img.object-cover').each( function() {
+      var thisObjectCoverImage = $(this);
+      var imageSrc = thisObjectCoverImage.attr('src');
+      // Hide original image and apply as background-image to parent
+      thisObjectCoverImage.hide();
+      thisObjectCoverImage.parent().addClass('image-cover').attr('style', 'background-image: url(' + imageSrc + ');');
+    });
+  }
+
+
+
+
+  // Footer menus open/close
+  function footerMobileMenus() {
+
+    if( window.innerWidth <= desktopBreakPoint ) { // Mobile
+      // Only show active menus
+      $('.footer-menu').children('ul').hide();
+      $('.footer-menu.active').children('ul').show();
+      // Click on menu heading
+      $('.footer-menu > h3').off().click(function(e) {
+        e.preventDefault();
+        var thisFooterMenu = $(this).parent('.footer-menu');
+        // Close all footer menus
+        $('.footer-menu').not(thisFooterMenu).children('ul').slideUp(mobileMenuSlideTime);
+        $('.footer-menu').not(thisFooterMenu).removeClass('active');
+        // Open selected menu
+        thisFooterMenu.children('ul').slideToggle(mobileMenuSlideTime);
+        thisFooterMenu.toggleClass('active');
+      });
+    }
+    else { // Desktop
+      // Show all menus
+      $('.footer-menu').children('ul').show();
+      // Prevent interaction with menu heading
+      $('.footer-menu > h3').off().click(function(e) {
+        e.preventDefault();
+      });
+    }
+
+  }
+  footerMobileMenus();
+  $(window).resize ( function() {	footerMobileMenus(); });
+
+
+
+
+
+
+
+
+  // Form labels
+  // On focus-on
+	$('.form-field input, .form-field select, .form-field textarea').focus( function() {
+		// Check other inputs
+		$(this).parent('.form-field').siblings('.form-field').each( function() {
+			if ($(this).children('input').val() == "") {
+				$(this).removeClass('open'); // Reset label if input is empty
+			}
+		});
+		// Minify current
+		$(this).parent('.form-field').addClass('open');
+	});
+	// On focus-off
+	$('.form-field input, .form-field select, .form-field textarea').blur( function() {
+			if (!$(this).val()) {
+				$(this).parent('.form-field').removeClass('open'); // Reset label if input is empty
+			}
+	});
+
+
+
+
+}); // end document.ready
---
+++ gulpfile.js
@@ -0,0 +1,197 @@
+// Requires
+const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass');
+sass.compiler = require('node-sass');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+const imageminMozjpeg = require('imagemin-mozjpeg');
+const t2 = require('through2');
+const clean = require('gulp-clean');
+const rename = require('gulp-rename');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
+
+// Paths variable to make src/dist easier
+const dev = './dev_theme';
+const compiled = './compiled_theme'
+
+const paths = {
+  // Dev root
+  dev: `${dev}`,
+  // Dev HTML
+  devHTMLDir: `${dev}/html`,
+  devHTML: `${dev}/html/**/*.html`,
+  devHTMLCSS: `${dev}/html/css`,
+  devHTMLJS: `${dev}/html/js`,
+  devHTMLImg: `${dev}/html/img`,
+  // Dev assets
+  devCSSDir: `${dev}/assets/css`,
+  devSCSS: `${dev}/assets/css/**/*.scss`,
+  devSCSSLiq: `${dev}/assets/css/*.scss.liquid`,
+  devCSSLiq: `${dev}/assets/css/*.css.liquid`,
+  devJS: `${dev}/assets/js/*.js`,
+  devJSLiq: `${dev}/assets/js/*.js.liquid`,
+  devImg: `${dev}/assets/img/**/*.{png,gif,jpg,svg}`, // include any of these image files types
+  // Dev liquid
+  devConfig: `${dev}/config/*`,
+  devLayout: `${dev}/layout/*.liquid`,
+  devLocales: `${dev}/locales/*`,
+  devSections: `${dev}/sections/*.liquid`,
+  devSnippets: `${dev}/snippets/*.liquid`,
+  devTemplates: `${dev}/templates/**/*.liquid`,
+  // Compiled root
+  compiled: `${compiled}`,
+  // Compiled Shopify directories
+  compiledAssets: `${compiled}/assets`,
+  compiledConfig: `${compiled}/config`,
+  compiledLayout: `${compiled}/layout`,
+  compiledLocales: `${compiled}/locales`,
+  compiledSections: `${compiled}/sections`,
+  compiledSnippets: `${compiled}/snippets`,
+  compiledTemplates: `${compiled}/templates`
+}
+
+// Run browser-sync on HTML change
+function pipeHTML() {
+  return gulp.src(paths.devHTML)
+    //.pipe(gulp.dest(paths.compiledHTML))
+    .pipe(browserSync.stream());
+}
+
+// Pass Liquid Files to the right place, all ES6 Arrow Functions
+const pipeConfig = () => gulp.src(paths.devConfig).pipe(gulp.dest(paths.compiledConfig));
+const pipeLayout = () => gulp.src(paths.devLayout).pipe(gulp.dest(paths.compiledLayout));
+const pipeSections = () => gulp.src(paths.devSections).pipe(gulp.dest(paths.compiledSections));
+const pipeSnippets = () => gulp.src(paths.devSnippets).pipe(gulp.dest(paths.compiledSnippets));
+const pipeTemplates = () => gulp.src(paths.devTemplates).pipe(gulp.dest(paths.compiledTemplates));
+
+
+const images = () => {
+  console.log('image processed');
+  return (
+    gulp
+    .src(paths.devImg)
+    .pipe(imagemin([
+      imagemin.gifsicle({
+        interlaced: true
+      }),
+      imageminMozjpeg({
+        quality: 70
+      }),
+      imagemin.optipng({
+        optimizationLevel: 5
+      }),
+      imagemin.svgo({
+        plugins: [{
+            removeViewBox: true
+          },
+          {
+            cleanupIDs: false
+          }
+        ]
+      })
+    ]))
+    .pipe(gulp.dest(paths.devHTMLImg)) // pipe to current dev location for local testing
+    .pipe(rename({dirname: ''})) // remove images parent directories for piping to compiled assets
+    .pipe(gulp.dest(paths.compiledAssets)) // pipe to compiled assets
+    .pipe(browserSync.stream())
+  )
+}
+
+function scss(cb) {
+  return (
+    gulp
+    .src(paths.devSCSS)
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }))
+    .on('error', sass.logError)
+    .pipe(autoprefixer({
+      overrideBrowserslist: ['last 10 versions'],
+      grid: true
+    }))
+    .pipe(t2.obj((chunk, enc, cb) => {
+      let date = new Date();
+      chunk.stat.atime = date;
+      chunk.stat.mtime = date;
+      cb(null, chunk);
+    }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(paths.devHTMLCSS)) // pipe to current dev location for local testing
+    .pipe(gulp.dest(paths.compiledAssets)) // pipe to compiled assets
+    .pipe(browserSync.stream())
+    //cb();
+  )
+}
+
+function scripts(cb) {
+  return (
+    gulp
+    .src(paths.devJS)
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(paths.devHTMLJS)) // pipe to current dev location for local testing
+    .pipe(gulp.dest(paths.compiledAssets)) // pipe to compiled assets
+    .pipe(browserSync.stream())
+    //cb();
+  )
+}
+
+function cssLiquid(cb) {
+  gulp.src([paths.devCSSLiq, paths.devSCSSLiq])
+  .pipe(gulp.dest(paths.compiledAssets))
+  cb();
+}
+
+function jsLiquid(cb) {
+  gulp.src(paths.devJSLiq)
+  .pipe(gulp.dest(paths.compiledAssets))
+  cb();
+}
+
+function configFiles(cb) {
+  gulp.src(paths.devConfig)
+  .pipe(gulp.dest(paths.compiledConfig))
+  cb();
+}
+function localesFiles(cb) {
+  gulp.src(paths.devLocales)
+  .pipe(gulp.dest(paths.compiledLocales))
+  cb();
+}
+
+
+
+
+
+/* This function runs everything and pipes to compiled */
+//gulp.task('compile', gulp.parallel(gulp.series(scss), gulp.series(scripts), gulp.parallel(pipeTemplates, pipeSections, pipeLayout, pipeSnippets, cssLiquid, jsLiquid), configFiles, localesFiles));
+
+/* GLOBAL WATCH FUNCTION */
+function watch() {
+
+  browserSync.init({
+    server: {
+      baseDir: paths.devHTMLDir // point server to base HTML
+    }
+    // reloadDelay: 1000, // manual delay to allow for ThemeKit to sync
+    // reloadOnRestart: true
+  });
+
+  gulp.watch(paths.devHTML, gulp.series(pipeHTML));
+  gulp.watch(paths.devSCSS, gulp.series(scss));
+  //gulp.watch(paths.devCSS, gulp.series(scss)).on('change', browserSync.reload);
+  gulp.watch(paths.devJS, gulp.series(scripts, jsLiquid));
+  gulp.watch(paths.devImg, gulp.series(images));
+  gulp.watch(`${paths.dev}/**/*.liquid`, gulp.parallel(pipeTemplates, pipeSections, pipeLayout, pipeSnippets, cssLiquid, jsLiquid));
+  //gulp.watch(`${paths.dev}/**/*.html`).on('change', browserSync.reload); // reload HTML
+}
+module.default = gulp.task('default', gulp.series(watch));
